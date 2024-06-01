78494eb41dbdf46809e41047d3b78641a2e1c2a0
Kamil Szydłowski <76235200+szydlovski@users.noreply.github.com>
2024-06-01 02:33:37 +0200

    feat: remove usused route, refactor

--- app/app/products/ListProductsContent.tsx
+++ app/app/products/ListProductsContent.tsx
@@ -26,6 +26,39 @@ const ProductTileSkeleton = () => (
 	</Card>
 );
 
+export const ProductTile = ({
+	product: { id, title, thumbnail, author },
+}: {
+	product: ListProductsRecord;
+}) => (
+	<Link key={id} href={`/app/products/${id}`}>
+		<Card>
+			<CardContent className="p-0 bg-neutral-100 aspect-square">
+				<div className="h-full w-full flex justify-center items-center p-2 md:p-4">
+					<Image
+						src={thumbnail.src}
+						alt={title}
+						width={thumbnail.width}
+						height={thumbnail.height}
+					/>
+				</div>
+			</CardContent>
+			<CardHeader className="p-2">
+				<CardTitle className="text-sm"> {title}</CardTitle>
+				<CardDescription className="text-xs">
+					Author: {author.name}
+				</CardDescription>
+			</CardHeader>
+		</Card>
+	</Link>
+);
+
+const ProductGridContainer = ({ children }: { children: React.ReactNode }) => (
+	<div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+		{children}
+	</div>
+);
+
 export const ListProductsContent = () => {
 	const { data, status } = useQuery<ListProductsRecord[]>({
 		queryKey: ['listProducts'],
@@ -37,38 +70,17 @@ export const ListProductsContent = () => {
 			{status === 'error' ? (
 				<>Error</>
 			) : (
-				<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
+				<ProductGridContainer>
 					{status === 'pending'
 						? Array(12)
 								.fill(0)
 								.map((_, i) => {
 									return <ProductTileSkeleton key={i} />;
 								})
-						: data.map(({ id, title, thumbnail, author }) => {
-								return (
-									<Link key={id} href={`/app/products/${id}`}>
-										<Card>
-											<CardContent className="p-0 bg-neutral-100 aspect-square">
-												<div className="h-full w-full flex justify-center items-center p-2 md:p-4">
-													<Image
-														src={thumbnail.src}
-														alt={title}
-														width={thumbnail.width}
-														height={thumbnail.height}
-													/>
-												</div>
-											</CardContent>
-											<CardHeader className="p-2">
-												<CardTitle className="text-sm"> {title}</CardTitle>
-												<CardDescription className="text-xs">
-													Author: {author.name}
-												</CardDescription>
-											</CardHeader>
-										</Card>
-									</Link>
-								);
-						  })}
-				</div>
+						: data.map((product) => (
+								<ProductTile key={product.id} product={product} />
+						  ))}
+				</ProductGridContainer>
 			)}
 		</DashboardViewLayout>
 	);
