1aaf61adb4ccb2ea1d347c4568aee6b55d8f84d1
Kamil Szydłowski <76235200+szydlovski@users.noreply.github.com>
2024-06-04 15:21:25 +0200

    feat: sidebar icon

--- components/layout/dashboard/Sidebar.tsx
+++ components/layout/dashboard/Sidebar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { cn } from '@/lib';
 import { usePathname } from 'next/navigation';
 import { MENU_LINKS } from '../links';
+import Image from 'next/image';
 
 export const Sidebar = () => {
 	const pathname = usePathname();
@@ -12,6 +13,7 @@ export const Sidebar = () => {
 			<div className="flex h-full max-h-screen flex-col gap-2">
 				<div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
 					<Link href="/" className="flex items-center gap-2 font-semibold">
+						<Image src='/icon-sm.png' width={24} height={24} alt='StitchStudio' />
 						<span className="">StitchStudio</span>
 					</Link>
 				</div>
