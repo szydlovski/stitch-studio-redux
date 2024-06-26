e85342ac710c9f107bb25338cbcad5981e2eea98
Kamil Szydłowski <76235200+szydlovski@users.noreply.github.com>
2024-06-26 18:37:36 +0200

    feat: wip

--- src/presentation/views/ProductView/ProductCards/LaunchChecklistCard.tsx
+++ src/presentation/views/ProductView/ProductCards/LaunchChecklistCard.tsx
@@ -18,6 +18,7 @@ import { cn } from '@lib/utils';
 import { VariantProps, cva } from 'class-variance-authority';
 import { ReactNode } from 'react';
 import { MockupSettingsModal } from './MockupSettingsModal';
+import { FullProductObject } from '@domain/product';
 
 export const checklistNumberVariants = cva(
 	'w-6 h-6 border rounded-full flex justify-center items-center text-sm font-semibold',
@@ -102,10 +103,83 @@ const ActiveChecklistItem = ({
 	);
 };
 
-interface ChecklistItemConfig {}
+interface ChecklistItemConfig {
+	key: LanuchItem;
+	label: string;
+	checked?: boolean;
+	content?: ReactNode;
+}
+
+enum LanuchItem {
+	UploadDesign = 'upload_design',
+	CustomizeMockup = 'customize_mockup',
+	CustomizePrintable = 'customize_printable',
+	CustomizeCovers = 'customize_covers',
+	PublishOnEtsy = 'publish_on_etsy',
+	Done = 'done',
+}
+
+const items: ChecklistItemConfig[] = [
+	{
+		key: LanuchItem.UploadDesign,
+		label: 'Upload design',
+		checked: true,
+	},
+	{
+		key: LanuchItem.CustomizeMockup,
+		label: 'Customize mockup',
+		content: (
+			<div className="grid gap-2 py-2">
+				<p className="text-sm">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</p>
+				<div>
+					<MockupSettingsModal>
+						<Button variant="outline">Open mockup settings</Button>
+					</MockupSettingsModal>
+				</div>
+			</div>
+		),
+	},
+	{
+		key: LanuchItem.CustomizePrintable,
+		label: 'Customize printable',
+		content: (
+			<div className="grid gap-2 py-2">
+				<p className="text-sm">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				</p>
+				<div>
+					<MockupSettingsModal>
+						<Button variant="outline">Open printable settings</Button>
+					</MockupSettingsModal>
+				</div>
+			</div>
+		),
+	},
+	{
+		key: LanuchItem.CustomizeCovers,
+		label: 'Customize covers',
+	},
+	{
+		key: LanuchItem.PublishOnEtsy,
+		label: 'Publish on Etsy',
+	},
+];
+
+const getActiveItem = (product: FullProductObject) => {
+	const { attributes } = product;
+	if (!attributes.hoopConfig) return LanuchItem.CustomizeMockup;
+	if (!attributes.printableConfig) return LanuchItem.CustomizePrintable;
+	if (!attributes.coverConfig) return LanuchItem.CustomizeCovers;
+	if (!attributes.etsyPublishedListingPayload) return LanuchItem.PublishOnEtsy;
+	return LanuchItem.Done;
+};
 
 export const LaunchChecklistCard = () => {
 	const { product, pattern } = useProductContext();
+	const activeItem = getActiveItem(product);
+	const activeIndex = items.findIndex((item) => item.key === activeItem);
 	return (
 		<Card className="bg-violet-600 text-white border-violet-600">
 			<CardHeader>
@@ -113,34 +187,23 @@ export const LaunchChecklistCard = () => {
 			</CardHeader>
 			<CardContent>
 				<div className="flex flex-col gap-4">
-					<ChecklistItem title="Upload design" number={1} status="completed" />
-					<ActiveChecklistItem
-						title="Customize mockup"
-						number={2}
-						status="active"
-					>
-						<div className="grid gap-2 py-2">
-							<p className="text-sm">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-							</p>
-							<div>
-								<MockupSettingsModal>
-									<Button variant="outline">Open mockup settings</Button>
-								</MockupSettingsModal>
-							</div>
-						</div>
-					</ActiveChecklistItem>
-					<ChecklistItem
-						title="Customize printable"
-						number={3}
-						status="inactive"
-					/>
-					<ChecklistItem
-						title="Customize covers"
-						number={4}
-						status="inactive"
-					/>
-					<ChecklistItem title="Publish on Etsy" number={5} status="inactive" />
+					{items.map(({ key, label, checked, content }, index) =>
+						key === activeItem ? (
+							<ActiveChecklistItem
+								title={label}
+								number={index + 1}
+								status="active"
+							>
+								{content}
+							</ActiveChecklistItem>
+						) : (
+							<ChecklistItem
+								title={label}
+								number={index + 1}
+								status={activeIndex >= index ? 'completed' : 'inactive'}
+							/>
+						)
+					)}
 				</div>
 			</CardContent>
 		</Card>
